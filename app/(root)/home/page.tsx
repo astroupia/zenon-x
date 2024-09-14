631f46638e12585eef882d1af0874f73a43b979b
astroupia <103548612+astroupia@users.noreply.github.com>
2024-09-14 22:16:17 +0300

    feat: home page setup

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,12 @@
+import MainHome from "@/view/main-home";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <MainHome />
+    </div>
+  );
+};
+
+export default page;
