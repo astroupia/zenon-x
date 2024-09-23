38797c2a8cf388f10fe8e9a00d5996d4046702b3
astroupia <103548612+astroupia@users.noreply.github.com>
2024-09-23 15:43:30 +0300

    feat: created project page

---
+++ app/(root)/team/programmer/project/page.tsx
@@ -0,0 +1,12 @@
+import DeveloperProject from "@/view/developer-project";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <DeveloperProject />
+    </div>
+  );
+};
+
+export default page;
