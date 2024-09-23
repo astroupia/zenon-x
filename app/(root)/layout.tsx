181083291a9072f3dca0a660e839e202ba400ffe
astroupia <103548612+astroupia@users.noreply.github.com>
2024-09-23 14:22:03 +0300

    fix: layout export module

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,11 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
-};
-
-export default Layout; // Changed 'layout' to 'Layout'
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
