1583fc1899178cec0efbb09752babd436fbf6a1f
zeamanuel fetene <zeamanuel1994@gmail.com>
2024-09-23 10:51:30 +0300

    it sholud be globals not global that the change  of the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import '../styles/global.css'; // Or '@/styles/global.css' if the alias is configured
+import '../styles/globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
