fa03ed2febce512a45f30425bc3eaed338b1c61d
astroupia <103548612+astroupia@users.noreply.github.com>
2024-09-23 15:42:44 +0300

    chore: used the right import for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,48 +1,13 @@
-'use client';
-
-import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar';
-import ProjectCard from '../components/Card';
-import styles from '../styles/Home.module.css';
+import styles from "@/styles/home.module.css";
 
 const Home: React.FC = () => {
-  const [selectedTech, setSelectedTech] = useState<string[]>([]);
-
-  const handleSelectTech = (tech: string) => {
-    setSelectedTech((prevSelectedTech) =>
-      prevSelectedTech.includes(tech)
-        ? prevSelectedTech.filter((t) => t !== tech)
-        : [...prevSelectedTech, tech]
-    );
-  };
-
   return (
-    <div className={styles.container}>
-      <Sidebar selectedTech={selectedTech} onSelectTech={handleSelectTech} />
-      <div className={styles.cardsContainer}>
-        {selectedTech.length > 0 ? (
-          selectedTech.map((tech) => <ProjectCard key={tech} tech={tech} />)
-        ) : (
-          <p>No projects selected.</p>
-        )}
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>ZEONO-X - Client View</h1>
       </div>
-    </div>
+    </main>
   );
 };
 
 export default Home;
-
-
-
-
-//i capt your codes just in case you wanna change it dawg 
-
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.description}>
-//         <h1>ZEONO-X - Client View</h1>
-//       </div>
-//     </main>
-//   );
-// }
